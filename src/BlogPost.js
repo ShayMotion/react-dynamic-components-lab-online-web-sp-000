5d48f7d51f064f1d28f3d7a4de6f18bcf427058a
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 15:22:11 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -12,18 +12,18 @@ export default class BlogPost extends Component {
         ]
 
         return (
-        <div id="blog-post" className="wrapper">
-            Alright internet trolls<br/>
-            Come at me<br/>
-            I have been preparing<br/>
-            My meme fortress is built<br/>
-            I won't let you tilt me<br/>
-            You only grew accustomed to the salt<br/>
-            I was born in it<br/>
-            <Comment commentText={comments[0]} />
-            <Comment commentText={comments[1]} />
-            <Comment commentText={comments[2]} />
-        </div>
+        <div id="blog-post" className="wrapper">;
+            Alright internet trolls<br/>;
+            Come at me<br/>;
+            I have been preparing<br/>;
+            My meme fortress is built<br/>;
+            I won't let you tilt me<br/>;
+            You only grew accustomed to the salt<br/>;
+            I was born in it<br/>;
+            <Comment commentText={comments[0]} />;
+            <Comment commentText={comments[1]} />;
+            <Comment commentText={comments[2]} />;
+        </div>;
         );
     }
 }
